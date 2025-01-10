8f3d138153ffe0504aba175252d26a568cd64c03
RakeShrestha <orakeshstha7@gmail.com>
2025-01-10 12:24:42 +0545

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Rakesh Shrestha on 10/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
